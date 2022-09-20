f7b2c3d57584c4a51dc175d5b2208956e3f6c20f
Andrew Erickson <aerickson@gmail.com>
2022-09-19 21:03:27 -0700

    pijuice: depend on proper package

--- packages/addons/service/pijuice-base/package.mk
+++ packages/addons/service/pijuice-base/package.mk
@@ -3,7 +3,7 @@ PKG_VERSION="1.8"
 # PKG_SITE=""
 PKG_URL="https://github.com/PiSupply/PiJuice/archive/V$PKG_VERSION.tar.gz"
 
-PKG_DEPENDS_TARGET="toolchain Python3 py-smbus"
+PKG_DEPENDS_TARGET="toolchain Python3 i2c-tools"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="pijuice-base"
