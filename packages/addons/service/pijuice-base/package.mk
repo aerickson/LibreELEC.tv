5a416299d4fcb0cc1d16bc23fef0c3e670c7173a
Andrew Erickson <aerickson@gmail.com>
2022-09-19 19:33:15 -0700

    re-enable pijuice

--- packages/addons/service/pijuice-base/package.mk
+++ packages/addons/service/pijuice-base/package.mk
@@ -3,8 +3,10 @@ PKG_VERSION="1.8"
 # PKG_SITE=""
 PKG_URL="https://github.com/PiSupply/PiJuice/archive/V$PKG_VERSION.tar.gz"
 
-PKG_DEPENDS_TARGET="toolchain Python3"
+PKG_DEPENDS_TARGET="toolchain Python3 smbus"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="pijuice"
 PKG_ADDON_TYPE="xbmc.service"
+
+PKG_TOOLCHAIN="manual"
