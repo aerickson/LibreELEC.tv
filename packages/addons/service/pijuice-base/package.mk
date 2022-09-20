10d5953e874b0c515db40ceead4718e270b9768a
Andrew Erickson <aerickson@gmail.com>
2022-09-19 23:34:46 -0700

    latest wip

--- packages/addons/service/pijuice-base/package.mk
+++ packages/addons/service/pijuice-base/package.mk
@@ -10,3 +10,43 @@ PKG_ADDON_NAME="pijuice-base"
 PKG_ADDON_TYPE="xbmc.service"
 
 PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+	# what to install:
+	#
+	#  * The low level interface (pijuice.py)
+	#    - Software/Source/pijuice.py
+	#  * The supporting service (pijuice_sys.py)
+	#    - Software/Source/src/pijuice_sys.py
+	#  * and the command line interface (pijuice_cli, pijuice_cli.py).
+	#    - Software/Source/src/pijuice_cli.py
+
+	# dest: /usr/bin/pijuice_sys.py
+
+	# TODO: place sys binary
+	install -m 755 ${PKG_BUILD}/Software/Source/src/pijuice_sys.py ${INSTALL}/usr/bin/
+	# TODO: place CLI
+	install -m 755 ${PKG_BUILD}/Software/Source/src/pijuice_cli.py ${INSTALL}/usr/bin/
+}
+
+# make_target() {
+#   rm -f Makefile
+#   LDFLAGS="${LDFLAGS} -lX11" ${MAKE} unclutter
+# }
+
+# makeinstall_target() {
+#   mkdir -p ${INSTALL}/usr/bin
+#   install -m 755 unclutter ${INSTALL}/usr/bin/
+# }
+
+post_install() {
+	# TODO: place i2c rules file?
+
+	# TODO: add pijuice user and group
+	# add_group systemd-network 193
+	# add_user systemd-network x 193 193 "systemd-network" "/" "/bin/sh"
+
+	# knows to look in ./system.d/
+	# does placement also... i think
+	enable_service pijuice.service
+}
