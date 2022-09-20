b1650cba8a3e17602d444c29e86fff101998759e
Andrew Erickson <aerickson@gmail.com>
2022-09-19 19:54:29 -0700

    pijuice tweak

--- packages/addons/service/pijuice-base/package.mk
+++ packages/addons/service/pijuice-base/package.mk
@@ -6,7 +6,7 @@ PKG_URL="https://github.com/PiSupply/PiJuice/archive/V$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python3 smbus"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="pijuice"
+PKG_ADDON_NAME="pijuice-base"
 PKG_ADDON_TYPE="xbmc.service"
 
 PKG_TOOLCHAIN="manual"
